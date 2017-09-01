461f0919ea63c5f831346c1332438990158f45cb
Niko Kristian Pinnis <niko.pinnis@gmail.com>
2017-09-02 00:27:39 +0300

    Add support for displaying duration of videos using ffmpeg

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
 
 app.on('ready', () => {
-  console.log('App is ready!');
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    // console.log('Video duration: ', metadata.format.duration);
+    mainWindow.webContents.send('video:metadata', metadata.format.duration);
+  });
 });
